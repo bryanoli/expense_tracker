c98e65a0207ff6c6b0295133c286e5050b962e3b
bryanoli <bryan.olivarez14@gmail.com>
2023-09-23 03:23:11 -0700

    Fixed secret leak

--- server/index.js
+++ server/index.js
@@ -1,88 +1,154 @@
 const express = require ("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const moment = require ('moment');
+const mongoose = require('mongoose');
 require('dotenv').config();
-const { Configuration, PlaidApi, PlaidEnvironments } = require('plaid');
 
-const configuration = new Configuration({
-  basePath: PlaidEnvironments.sandbox,
-  baseOptions: {
-    headers: {
-        'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
-        'PLAID-SECRET': process.env.PLAID_SECRET,
-    },
-  },
-});
-
-
-const plaidClient = new PlaidApi(configuration);
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })) //Need it for time format
 
+const mongoURI = process.env.MONGODB_URI;
 
-//First endpoint
-app.post("/hello",(request,response)=> {
-    response.json({message:"hello " + request.body.name});
+const mongooseOptions = {
+  useNewUrlParser: true, // Use the new URL parser
+  useUnifiedTopology: true, // Use the new Server Discover and Monitoring engine
+};
+
+mongoose.connect(mongoURI, mongooseOptions)
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  });
+
+const db = mongoose.connection;
+
+let clientSchema = mongoose.Schema({
+  email: String,
+  password: String,
+  transactions: Array,
+  items: Array
 });
 
-app.post('/create_link_token', async function (request, response) {
-    // Get the client_user_id by searching for the current user
-    const plaidRequest = {
-      user: {
-        // This should correspond to a unique id for the current user.
-        client_user_id: 'user',
-      },
-      client_name: 'Plaid Test App',
-      products: ['auth'],
-      language: 'en',
-      redirect_uri: 'http://localhost:5173/',
-      country_codes: ['US'],
-    };
-    try {
-      const createTokenResponse = await plaidClient.linkTokenCreate(plaidRequest);
-      response.json(createTokenResponse.data);
-    } catch (error) {
-        response.status(500).send("failure");
-      // handle error
+let Client = mongoose.model('Client', clientSchema);
+
+app.post('/login', (req, res) => {
+  let { email, password } = req.body;
+  Client.findOne({email,password}, (err,doc)=>{
+    if(err){
+      res.sendStatus(500);
+      return;
     }
+    res.sendStatus({id:doc._id})
   });
 
-  app.post("/auth", async function(request,response){
-    try{
-        const access_token = request.body.access_token;
-        const plaidRequest = {
-            access_token: access_token,
-        };
-        const plaidResponse = await plaidClient.authGet(plaidRequest);
-        response.json(plaidResponse.data);
-    }catch (e){
-        response.status(500).send("failed");
-    }
-  });
+});
 
-  app.post('/exchange_public_token', async function (
-    request,
-    response,
-    next,
-  ) {
-    const publicToken = request.body.public_token;
-    try {
-      const plaidResponse = await plaidClient.itemPublicTokenExchange({
-        public_token: publicToken,
-      });
-  
-      // These values should be saved to a persistent database and
-      // associated with the currently signed-in user
-      const accessToken = plaidResponse.data.access_token;
-    //   const itemID = response.data.item_id;
-  
-      response.json({ accessToken });
-    } catch (error) {
-        response.status(500).send("Failed");
-      // handle error
-    }
+app.post('/signup', (req, res) => {
+  let { email, password } = req.body;
+  let newClient = new Client({ email, password });
+  newClient.save((err, client)=>{
+    res.send({message:`User created with ID: ${user._id}`})
   });
+});
+
+//THIS IS FOR PLAID
+// const moment = require ('moment');
+
+
+// const { Configuration, PlaidApi, PlaidEnvironments } = require('plaid');
+
+// const configuration = new Configuration({
+//   basePath: PlaidEnvironments.sandbox,
+//   baseOptions: {
+//     headers: {
+//         'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+//         'PLAID-SECRET': process.env.PLAID_SECRET,
+//     },
+//   },
+// });
+
+
+// const plaidClient = new PlaidApi(configuration);
+
+// db.on('connected', () => {
+//   console.log('Mongoose connected to MongoDB Atlas');
+// });
+
+// db.on('error', (error) => {
+//   console.error('Mongoose connection error:', error);
+// });
+
+// db.on('disconnected', () => {
+//   console.log('Mongoose disconnected from MongoDB Atlas');
+// });
+
+//First endpoint
+// app.post("/hello",(request,response)=> {
+//     response.json({message:"hello " + request.body.name});
+// });
+
+// app.post('/create_link_token', async function (request, response) {
+//     // Get the client_user_id by searching for the current user
+//     const plaidRequest = {
+//       user: {
+//         // This should correspond to a unique id for the current user.
+//         client_user_id: 'user',
+//       },
+//       client_name: 'Plaid Test App',
+//       products: ['auth'],
+//       language: 'en',
+//       redirect_uri: 'http://localhost:5173/',
+//       country_codes: ['US'],
+//     };
+//     try {
+//       const createTokenResponse = await plaidClient.linkTokenCreate(plaidRequest);
+//       response.json(createTokenResponse.data);
+//     } catch (error) {
+//         response.status(500).send("failure");
+//       // handle error
+//     }
+//   });
+
+//   app.post("/auth", async function(request,response){
+//     try{
+//         const access_token = request.body.access_token;
+//         const plaidRequest = {
+//             access_token: access_token,
+//         };
+//         const plaidResponse = await plaidClient.authGet(plaidRequest);
+//         response.json(plaidResponse.data);
+//     }catch (e){
+//         response.status(500).send("failed");
+//     }
+//   });
+
+//   app.post('/exchange_public_token', async function (
+//     request,
+//     response,
+//     next,
+//   ) {
+//     const publicToken = request.body.public_token;
+//     try {
+//       const plaidResponse = await plaidClient.itemPublicTokenExchange({
+//         public_token: publicToken,
+//       });
+  
+//       // These values should be saved to a persistent database and
+//       // associated with the currently signed-in user
+//       const accessToken = plaidResponse.data.access_token;
+//     //   const itemID = response.data.item_id;
+  
+//       response.json({ accessToken });
+//     } catch (error) {
+//         response.status(500).send("Failed");
+//       // handle error
+//     }
+//   });
 
 
 app.listen(8000,()=> {
